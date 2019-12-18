47d7e16645f4b4048eb853f9af4058f4881fca93
dean <dean@dean.com>
2019-12-17 23:36:12 -0600

    Add configuration variable for tor proxy address

--- config.go
+++ config.go
@@ -6,6 +6,7 @@ import (
 
 type Config struct {
 	Tor struct {
+		ProxyAddress      string `json:"proxy_address"`
 		ControllerAddress string `json:"controller_address"`
 	} `json:"tor_config"`
 	ServerAddress string `json:"server_address"`
@@ -14,6 +15,7 @@ type Config struct {
 
 //Me being cheeky
 var configDir = os.Getenv("HOME") + "/.config/tchatd"
+
 func ConfigPath(filename string) string {
 	return configDir + "/" + filename
 }
