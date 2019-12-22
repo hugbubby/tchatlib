13866df627187600532639ab7a0c54cc419988e2
dean <dean@dean.com>
2019-12-21 19:02:19 -0600

    New message structure

--- messages.go
+++ messages.go
@@ -1,8 +1,20 @@
 package tchatlib
 
-import "golang.org/x/crypto/ed25519"
+import (
+	"time"
+
+	"golang.org/x/crypto/ed25519"
+)
 
 type Message struct {
-	Sender  ed25519.PublicKey `json:"sender"`
-	Content string            `json:"content"`
+	Sender    ed25519.PublicKey `json:"sender"`
+	Signature []byte            `json:"signature"`
+	Content   string            `json:"content"`
+}
+
+type MessageLogFile struct {
+	MessageLogs []struct {
+		Message
+		time.Time
+	} `json:"message_logs"`
 }
