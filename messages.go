ea06a5814149463793c63c2fbbe66e573c403f7b
dean <dean@dean.com>
2019-12-21 18:51:05 -0600

    No more contacts

--- messages.go
+++ messages.go
@@ -3,14 +3,6 @@ package tchatlib
 import "golang.org/x/crypto/ed25519"
 
 type Message struct {
-	ServiceID string `json:"service_id"`
-	Content   string `json:"content"`
-}
-
-type ContactList struct {
-	Contacts map[string]ContactDetails `json:"contacts"`
-}
-
-type ContactDetails struct {
-	PubKey    ed25519.PublicKey `json:"pubkey"`
+	Sender  ed25519.PublicKey `json:"sender"`
+	Content string            `json:"content"`
 }
